c2b9cfc9d6c8b772f8ea648c315146707bedb514
lucamjdimarco <lucadm24@gmail.com>
2024-09-03 12:37:01 +0200

    Some fix

--- utils/func_aux.h
+++ utils/func_aux.h
@@ -1,5 +1,5 @@
-//#ifdef _FUNC_AUX_H_
-//#define _FUNC_AUX_H_
+#ifndef FUNC_AUX_H
+#define FUNC_AUX_H
 
 struct monitored_entry {
     char *path;
@@ -16,4 +16,4 @@ char *get_absolute_path(const char *user_path);
 int scan_directory(const char *dir_path, struct monitored_entry **entries);
 static int is_directory(const char *path);
 
-//#endif
+#endif
